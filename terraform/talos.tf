fb5306d73e78c39d38e7d05928bac0a003800cc9
IloveNooodles <mgarebaldhie80@gmail.com>
2025-09-16 18:13:36 +0900

    feat: finish longhorn config :"D

--- terraform/talos.tf
+++ terraform/talos.tf
@@ -1,28 +1,53 @@
-resource "proxmox_virtual_environment_download_file" "talos_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = var.proxmox_node_name
+locals {
+  nodes = {
+    "talos-control-plane" = {
+      "node_name" = var.proxmox_node_name
+      "vm_id"     = 110
+      "cpu"       = 2
+      "tags"      = ["kubernetes", "control-plane"]
+    }
+    "talos-worker-0" = {
+      "node_name" = var.proxmox_node_name
+      "vm_id"     = 111
+      "cpu"       = 2
+      "tags"      = ["kubernetes", "worker"]
+    }
+  }
 
-  url = "https://factory.talos.dev/image/b35305d8b7a21098b24575a51aaf72bd8cc0e1f527653ad20735d3addf09a5f7/v1.11.1/metal-amd64.iso"
+  bootable = {
+    type      = "iso"
+    file_name = "talos-v1.11.1.iso"
+    url       = "https://factory.talos.dev/image/b1ba84be4f5193a24085cc7e22fce31105e1583504d7d5aef494318f7cb1abd0/v1.11.1/metal-amd64.iso"
+  }
 }
 
-resource "proxmox_virtual_environment_vm" "talos_template" {
-  name      = "talos-template"
-  node_name = var.proxmox_node_name
-  tags      = ["terraform", "talos", "template"]
+resource "proxmox_virtual_environment_download_file" "talos_image" {
+  content_type = local.bootable.type
+  datastore_id = "local"
+  node_name    = var.proxmox_node_name
+  file_name    = local.bootable.file_name
 
-  template = true
-  started  = false
+  url = local.bootable.url
+}
 
+resource "proxmox_virtual_environment_vm" "talos" {
+  for_each = local.nodes
+  tags     = each.value.tags
+
+  name        = each.key
+  node_name   = each.value.node_name
+  vm_id       = each.value.vm_id
   bios        = "seabios"
   description = "Managed by Terraform"
+  started     = false
+  template    = false
 
   agent {
     enabled = true
   }
 
   cpu {
-    cores = 2
+    cores = each.value.cpu
     type  = "x86-64-v2-AES"
   }
 
@@ -49,50 +74,5 @@ resource "proxmox_virtual_environment_vm" "talos_template" {
 
   network_device {
     bridge = "vmbr0"
-  }
-
-}
-
-locals {
-  nodes = {
-    "talos-control-plane" = {
-      "node_name" = var.proxmox_node_name
-      "vm_id"     = 102
-      "cpu"       = 2
-      "tags"      = ["kubernetes", "control-plane"]
-    }
-    "talos-worker-0" = {
-      "node_name" = var.proxmox_node_name
-      "vm_id"     = 103
-      "cpu"       = 2
-      "tags"      = ["kubernetes", "worker"]
-    }
-  }
-}
-
-resource "proxmox_virtual_environment_vm" "talos" {
-  for_each = local.nodes
-  tags     = each.value.tags
-
-  name      = each.key
-  node_name = each.value.node_name
-  vm_id     = each.value.vm_id
-
-  clone {
-    vm_id = proxmox_virtual_environment_vm.talos_template.id
-  }
-
-  agent {
-    # NOTE: The agent is installed and enabled as part of the cloud-init configuration in the template VM, see cloud-config.tf
-    # The working agent is *required* to retrieve the VM IP addresses.
-    # If you are using a different cloud-init configuration, or a different clone source
-    # that does not have the qemu-guest-agent installed, you may need to disable the `agent` below and remove the `vm_ipv4_address` output.
-    # See https://registry.terraform.io/providers/bpg/proxmox/latest/docs/resources/virtual_environment_vm#qemu-guest-agent for more details.
-    enabled = true
-  }
-
-  cpu {
-    cores = each.value.cpu
-    type  = "x86-64-v2-AES"
   }
 }
