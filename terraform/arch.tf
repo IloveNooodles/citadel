115d252e244581a1b5a74ef4daaeb5f5ea71f481
IloveNooodles <mgarebaldhie80@gmail.com>
2025-09-26 01:14:07 +0900

    feat: add arch linux with omarchy

---
+++ terraform/arch.tf
@@ -0,0 +1,76 @@
+locals {
+  configuration = {
+    "omarchy" = {
+      "name"      = "gawrgare-omarchy"
+      "node_name" = var.proxmox_node_name
+      "vm_id"     = 101
+      "cpu"       = 2
+      "tags"      = ["arch", "omarchy"]
+      "memory"    = 4096
+      "disk"      = 50
+    }
+    "bootable" = {
+      type      = "iso"
+      file_name = "omarchy-3.0.1.iso"
+      url       = "https://iso.omarchy.org/omarchy-3.0.1.iso"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_download_file" "omarchy_image" {
+  content_type = local.configuration.bootable.type
+  datastore_id = "local"
+  node_name    = local.configuration.omarchy.node_name
+  file_name    = local.configuration.bootable.file_name
+
+  url = local.configuration.bootable.url
+}
+
+resource "proxmox_virtual_environment_vm" "omarchy" {
+  tags = local.configuration.omarchy.tags
+
+  name        = local.configuration.omarchy.name
+  node_name   = local.configuration.omarchy.node_name
+  vm_id       = local.configuration.omarchy.vm_id
+  bios        = "seabios"
+  description = "Managed by Terraform"
+  on_boot     = true
+
+  agent {
+    enabled = false
+  }
+
+  cpu {
+    cores = local.configuration.omarchy.cpu
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = local.configuration.omarchy.memory
+  }
+
+  disk {
+    datastore_id = var.proxmox_datastore_id
+    file_id      = proxmox_virtual_environment_download_file.omarchy_image.id
+    interface    = "scsi0"
+    iothread     = true
+    discard      = "on"
+    size         = local.configuration.omarchy.disk
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
